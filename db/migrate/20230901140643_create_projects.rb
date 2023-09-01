6dcd79325621f9826b435215da2d9805af654df1
yonas44 <tesfu.yonas1350@gmail.com>
2023-09-01 17:36:08 +0300

    Set up project model, specs and controller

---
+++ db/migrate/20230901140643_create_projects.rb
@@ -0,0 +1,13 @@
+class CreateProjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.text :description
+      t.integer :priority
+      t.references :user, null: false, foreign_key: true
+      t.boolean :completed, default: false
+
+      t.timestamps
+    end
+  end
+end
