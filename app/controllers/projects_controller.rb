37fab1a6b3bcca3ed1e56e2fc03ab407ca36a79b
yonas44 <tesfu.yonas1350@gmail.com>
2023-09-02 22:08:24 +0300

    Update application controller and projects controllers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,25 @@
 class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def create
+    @project = Project.new(project_params)
+    
+    if @project.save
+      flash.now[:success] = "Project successfully created."
+    else
+      flash.now[:error]  = "Project creation failed."
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :due_date, :priority, :completed)
+  end
 end
