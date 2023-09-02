22ace421ea605eff6f3575e9925c24be40d4592e
yonas44 <tesfu.yonas1350@gmail.com>
2023-09-02 22:09:16 +0300

    Set up routes.rb and update projects_spec.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   
   has_many :projects, through: :project_users
   has_many :project_users, dependent: :destroy
+
+  validates :full_name, presence: true
 end
--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Projects", type: :request do
+  before(:all) do
+    FactoryBot.create(:project, title: 'first')
+  end
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns all projects and render index.html' do
+      get projects_path
+      expect(response.body).to include('first')
+    end
+  end
+
+  describe "POST /create" do
+    it 'creates a record and returns a flash' do
+      post projects_path, params: { project: {title: 'second', description: 'second project'}}
+      expect(response.body).to include('first')
+    end
   end
 end
