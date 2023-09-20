0132e6ddb5cdae68a3ba2335e9076a4858596481
yonas44 <tesfu.yonas1350@gmail.com>
2023-09-20 22:35:30 +0300

    Update project and tasks migrations and create task_users table migration

--- db/migrate/20230901140643_create_projects.rb
+++ db/migrate/20230901140643_create_projects.rb
@@ -3,9 +3,10 @@ class CreateProjects < ActiveRecord::Migration[7.0]
     create_table :projects do |t|
       t.string :title
       t.text :description
-      t.integer :priority
       t.boolean :completed, default: false
       t.boolean :public, default: false
+      t.date :start_date
+      t.date :due_date
 
       t.timestamps
     end
