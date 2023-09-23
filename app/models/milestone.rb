69b77f9797a863322e8fefde96c00e696c6c574e
yonas44 <tesfu.yonas1350@gmail.com>
2023-09-23 18:15:38 +0300

    Create custome methods for milestone and project modals

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -3,4 +3,18 @@ class Milestone < ApplicationRecord
   has_many :tasks, dependent: :destroy
 
   validates :title, presence: true
+  validate :start_date_cannot_be_in_the_past
+  validate :due_date_after_start_date
+
+  def start_date_cannot_be_in_the_past
+    if start_date.present? && start_date < Date.current
+      errors.add(:start_date, "can't be in the past")
+    end
+  end
+
+  def due_date_after_start_date
+    if start_date.present? && due_date.present? && due_date <= start_date
+      errors.add(:due_date, "must be after the start date")
+    end
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,19 @@ class Project < ApplicationRecord
   has_many :milestones, dependent: :destroy
 
   validates :title, :description, presence: true
+  
+  def total_tasks
+    Task.joins(milestone: :project).where(projects: { id: self.id }).count
+  end
+
+  def total_completed_tasks
+    Task.joins(milestone: :project)
+        .where(projects: { id: self.id })
+        .where(status: 'Completed')
+        .count
+  end
+
+  def completion_percentage
+    total_tasks > 0 ? (total_completed_tasks.to_f / total_tasks.to_f * 100).round(2) : 0
+  end
 end
