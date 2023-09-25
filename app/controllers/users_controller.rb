89397a8dd2e859491cc7bd18a9754c03e4bca522
yonas44 <tesfu.yonas1350@gmail.com>
2023-09-25 12:55:34 +0300

    Update users_controller and profile_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,33 @@
 class UsersController < ApplicationController
+  before_action :find_user, except: [:create]
+
   def show
     @user
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html
+      else
+        format.turbo_stream
+      end
+    end
   end
 
   private
 
   def find_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:profession, :full_name, :bio)
   end
 end
